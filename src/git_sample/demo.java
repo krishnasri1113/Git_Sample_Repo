eda403cd27f42b0c9fec82cf47c4caec11ad567e
krishnasri1113 <krishnasri.devisetti1113@gmail.com>
2020-04-13 21:46:05 -0700

    Third commit in git sample

--- src/git_sample/demo.java
+++ src/git_sample/demo.java
@@ -6,6 +6,7 @@ public class demo {
 		// TODO Auto-generated method stub
 		System.out.println("I am learning git today");
 		System.out.println("I am learning git in the TekArch");
+		System.out.println("java sessions are completed");
 
 	}
 
