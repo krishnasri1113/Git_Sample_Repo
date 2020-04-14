0125dfea722a2776ff1457fb5364b8862494656c
krishnasri1113 <krishnasri.devisetti1113@gmail.com>
2020-04-13 23:17:36 -0700

    first commit insrihan branch

--- src/git_sample/demo.java
+++ src/git_sample/demo.java
@@ -7,6 +7,6 @@ public class demo {
 		System.out.println("I am learning git today");
 		System.out.println("I am learning git in the TekArch");
 		System.out.println("java sessions are completed");
-		System.out.println("I am making changes in chintu branch");
+	    System.out.println("I am on srihan branch");
 		}
 }
